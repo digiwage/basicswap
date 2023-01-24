f22f2377e7c52031d44585abe638b15a97e61740
gdiscord <43574012+gdiscord@users.noreply.github.com>
2023-01-24 17:09:25 +0000

    Update wage.py

--- basicswap/interface/wage.py
+++ basicswap/interface/wage.py
@@ -5,13 +5,22 @@
 # Distributed under the MIT software license, see the accompanying
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 
+from io import BytesIO
+
 from .btc import BTCInterface
 from basicswap.chainparams import Coins
 from basicswap.util.address import decodeAddress
-from mnemonic import Mnemonic
+from .contrib.pivx_test_framework.messages import (
+    CBlock,
+    ToHex,
+    FromHex,
+    CTransaction)
 from basicswap.contrib.test_framework.script import (
     CScript,
-    OP_DUP, OP_HASH160, OP_EQUALVERIFY, OP_CHECKSIG
+    OP_DUP,
+    OP_HASH160,
+    OP_CHECKSIG,
+    OP_EQUALVERIFY,
 )
 
 
@@ -20,43 +29,64 @@ class WAGEInterface(BTCInterface):
     def coin_type():
         return Coins.WAGE
 
-    def __init__(self, coin_settings, network, swap_client=None):
-        super().__init__(coin_settings, network, swap_client)
-        self._wallet_passphrase = ''
-        self._have_checked_seed = False
+    def signTxWithWallet(self, tx):
+        rv = self.rpc_callback('signrawtransaction', [tx.hex()])
+        return bytes.fromhex(rv['hex'])
 
-    def seedToMnemonic(self, key):
-        return Mnemonic('english').to_mnemonic(key)
+    def createRawFundedTransaction(self, addr_to: str, amount: int, sub_fee: bool = False, lock_unspents: bool = True) -> str:
+        txn = self.rpc_callback('createrawtransaction', [[], {addr_to: self.format_amount(amount)}])
+        fee_rate, fee_src = self.get_fee_rate(self._conf_target)
+        self._log.debug(f'Fee rate: {fee_rate}, source: {fee_src}, block target: {self._conf_target}')
+        options = {
+            'lockUnspents': lock_unspents,
+            'feeRate': fee_rate,
+        }
+        if sub_fee:
+            options['subtractFeeFromOutputs'] = [0,]
+        return self.rpc_callback('fundrawtransaction', [txn, options])['hex']
 
-    def initialiseWallet(self, key):
-        words = self.seedToMnemonic(key)
-
-        mnemonic_passphrase = ''
-        self.rpc_callback('upgradetohd', [words, mnemonic_passphrase, self._wallet_passphrase])
-        self._have_checked_seed = False
-        if self._wallet_passphrase != '':
-            self.unlockWallet(self._wallet_passphrase)
+    def createRawSignedTransaction(self, addr_to, amount) -> str:
+        txn_funded = self.createRawFundedTransaction(addr_to, amount)
+        return self.rpc_callback('signrawtransaction', [txn_funded])['hex']
 
     def decodeAddress(self, address):
         return decodeAddress(address)[1:]
 
-    def checkExpectedSeed(self, key_hash):
-        try:
-            rv = self.rpc_callback('dumphdinfo')
-            entropy = Mnemonic('english').to_entropy(rv['mnemonic'].split(' '))
-            entropy_hash = self.getAddressHashFromKey(entropy)[::-1].hex()
-            self._have_checked_seed = True
-            return entropy_hash == key_hash
-        except Exception as e:
-            self._log.warning('checkExpectedSeed failed: {}'.format(str(e)))
-        return False
+    def getBlockWithTxns(self, block_hash):
+        # TODO: Bypass decoderawtransaction and getblockheader
+        block = self.rpc_callback('getblock', [block_hash, False])
+        block_header = self.rpc_callback('getblockheader', [block_hash])
+        decoded_block = CBlock()
+        decoded_block = FromHex(decoded_block, block)
+
+        tx_rv = []
+        for tx in decoded_block.vtx:
+            tx_dec = self.rpc_callback('decoderawtransaction', [ToHex(tx)])
+            tx_rv.append(tx_dec)
+
+        block_rv = {
+            'hash': block_hash,
+            'tx': tx_rv,
+            'confirmations': block_header['confirmations'],
+            'height': block_header['height'],
+            'version': block_header['version'],
+            'merkleroot': block_header['merkleroot'],
+        }
+
+        return block_rv
 
     def withdrawCoin(self, value, addr_to, subfee):
-        params = [addr_to, value, '', '', subfee, False, False, self._conf_target]
+        params = [addr_to, value, '', '', subfee]
         return self.rpc_callback('sendtoaddress', params)
 
     def getSpendableBalance(self):
         return self.make_int(self.rpc_callback('getwalletinfo')['balance'])
+
+    def loadTx(self, tx_bytes):
+        # Load tx from bytes to internal representation
+        tx = CTransaction()
+        tx.deserialize(BytesIO(tx_bytes))
+        return tx
 
     def getScriptForPubkeyHash(self, pkh: bytes) -> bytearray:
         # Return P2PKH
@@ -69,6 +99,11 @@ class WAGEInterface(BTCInterface):
         self._log.info(f'BLockSpendTx  fee_rate, size, fee: {fee_rate}, {size}, {pay_fee}.')
         return pay_fee
 
+    def signTxWithKey(self, tx: bytes, key: bytes) -> bytes:
+        key_wif = self.encodeKey(key)
+        rv = self.rpc_callback('signrawtransaction', [tx.hex(), [], [key_wif, ]])
+        return bytes.fromhex(rv['hex'])
+
     def findTxnByHash(self, txid_hex: str):
         # Only works for wallet txns
         try:
@@ -79,16 +114,4 @@ class WAGEInterface(BTCInterface):
         if 'confirmations' in rv and rv['confirmations'] >= self.blocks_confirmed:
             block_height = self.getBlockHeader(rv['blockhash'])['height']
             return {'txid': txid_hex, 'amount': 0, 'height': block_height}
-
         return None
-
-    def unlockWallet(self, password: str):
-        super().unlockWallet(password)
-        # Store password for initialiseWallet
-        self._wallet_passphrase = password
-        if not self._have_checked_seed:
-            self._sc.checkWalletSeed(self.coin_type())
-
-    def lockWallet(self):
-        super().lockWallet()
-        self._wallet_passphrase = ''
